fbf9757eda749ed194f4f0393450982ea212e991
Salmane Nguyen <Salmane.nguyen@etu.univ-poitiers.fr>
2023-05-30 17:59:40 +0000

    vue detail equipement

--- it/urls.py
+++ it/urls.py
@@ -3,4 +3,5 @@ from it  import views
 
 urlpatterns = [
     path('machines', views.machine_list_view, name='list_machines'),
+    path('machine/<pk>', views.machine_detail_view, name='detail_machine'),
 ]
--- it/views.py
+++ it/views.py
@@ -4,5 +4,10 @@ from .models import Machine
 
 def machine_list_view(request):
     machines = Machine.objects.all()
-    context = {'machines': machines, 'head_title' : "Liste des machines"}
+    context = {'machines': machines, 'head_title' : "Liste des Equipements"}
     return render(request, 'machine_list.html', context)
+
+def machine_detail_view(request, pk):
+    machine = Machine.objects.get(id=pk)
+    context = {'machine' : machine, 'head_title' : "Detail Equipement"}
+    return render(request, 'machine_detail.html', context)
