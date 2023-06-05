57271a5864517e02e01930de6d8c1bd4b9a101de
Salmane Nguyen <Salmane.nguyen@etu.univ-poitiers.fr>
2023-06-05 00:04:04 +0000

    model personnel employe

--- it/models.py
+++ it/models.py
@@ -20,3 +20,12 @@ class Machine(models.Model):
 
     def __str__(self):
         return self.nom
+
+class Personne(models.Model):
+    id = models.AutoField(primary_key= True, editable = False)
+    prenom = models.CharField(max_length=20)
+    nom = models.CharField(max_length=20)
+    poste = models.CharField(max_length=20)
+
+    def __str__(self):
+        return '%s %s'%(self.nom, self.prenom)
